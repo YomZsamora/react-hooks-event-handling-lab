a399416a60f1ed0fdab66c670dd289793e7b7239
yomZsamora <samora.y@adzumi.co.ke>
2022-03-27 20:18:40 +0300

    Completed Lab.

--- src/components/EyesOnMe.js
+++ src/components/EyesOnMe.js
@@ -3,6 +3,14 @@ import React from "react";
 
 function EyesOnMe() {
 
+   let watching = () => console.log(`Good!`)
+   let distracted = () => console.log(`Hey! Eyes on me!`) 
+
+   return (
+      <React.Fragment>
+         <button onFocus={watching} onBlur={distracted}>Eyes on me</button>
+      </React.Fragment>
+   )
 }
 
 export default EyesOnMe;
